2f57a53591f8f558004053460e1f4bddd2d4b042
snehalkoli <67510836+snehalkoli@users.noreply.github.com>
2022-11-17 18:21:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 3, 2020</small>
+    </div>
+  );
+}
